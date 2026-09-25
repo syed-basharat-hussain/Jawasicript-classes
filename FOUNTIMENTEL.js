d50230d6d67c0758a007ba25ad795ec5f6bd9aaf
Syed Basharat Hussain <syedbasharathussain49@gmail.com>
2026-09-25 18:06:38 +0500

    Add my task

--- FOUNTIMENTEL.js
+++ FOUNTIMENTEL.js
@@ -1,9 +1,118 @@
 
-let age = 22;
-if (age >= 18) {
-    console.log("Eligible to vote");
-}
-else if (age < 18){
-    console.log("Not eligible to vote");
+// let age = 14;
+// if (age >= 18) {
+//     console.log("Eligible to vote");
+// }
+// else if (age < 18){
+//     console.log("Not eligible to vote");
+    
+// }
+
+// let a = -2;
+// if (a >= 0){
+//     console.log("a is Positive");
+    
+// }
+// else {
+//     console.log("a is Negative");
+    
+// }
+
+// for (let a=2; a<=100; ++a){
+
+//     console.log(a);
+    
+// }
+// let a = 1
+// for (let a=10; a<=5; ++a){
+
+//     console.log(a);
+// }
+
+// for (let a=100; a>=1; a = a-5) {
+//     console.log(a);
+    
+// }
+
+// for (let a=1; a<=100; a = a + 2) {
+//     console.log(a);
+    
+// }
+
+// for (let a=2; a<=100; a = a + 2){
+//     console.log(a);
+    
+// }
+// for (let a=3; a<=100; a = a + 3){
+//     console.log(a);
+    
+// }
+// let a = 1;
+// for (let a=1; a<=100; a++){
+//     console.log(a);
+    
+// }
+
+// let a = 9;
+// if (a > 0) {
+//     console.log("positive");
+    
+// }
+// else if (a < 0) {
+//     console.log("Negative");
+// }
+// else if (a === 0) {
+//     console.log("Zero");
+    
+// }
+
+// let a = 37;
+// if (a >= 80 && a <= 100) {
+//     console.log("A+");
+    
+// }
+// if (a >= 70 && a < 80) {
+//     console.log("A");
+    
+// }
+// if (a >= 60 && a < 70) {
+//     console.log("B");
+    
+// }
+// if (a >= 50 && a < 60) {
+//     console.log("C");
+    
+// }
+// if (a >= 40 && a < 50) {
+//     console.log("D");
+    
+// }
+// if (a < 40) {
+//     console.log("Fail");
+    
+// }
+
+// let age = 61;
+// if (age <= 13) {
+//     console.log("Child ");
+    
+// }
+// else if (age > 13 && age <= 19) {
+//     console.log("Teenager");
+    
+// }
+// else if (age > 19 && age <= 59) {
+//     console.log("Adult ");
+    
+// }
+// else if (age >= 60) {
+//     console.log("Senior Citizen ");
+    
+// }
+
+let num = 20;
+If (num % 2 === 0) {
+    console.log("Even");
     
 }
+else ("Odd")
